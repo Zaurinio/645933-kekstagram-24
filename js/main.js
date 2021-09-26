a05bfc247aeb5b417ed8d1a43699e35e4457748d
Заур <zaurinio@inbox.ru>
2021-09-26 15:14:27 +0300

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // пример вычисления взят из источника: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomNumber (min, max) {
-  return (min >= 0 && max >= 0) ? Math.floor(Math.random() * (max - min + 1)) + min : console.log('Диапазон может быть только положительный, включая ноль');
+  return (min >= 0 && max >= 0) ? Math.floor(Math.random() * (max - min + 1)) + min : 'Диапазон может быть только положительный, включая ноль';
 }
 
 getRandomNumber(0, 20);
